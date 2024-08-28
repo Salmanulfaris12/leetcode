a278b2f767fa90c2056a8c553abcd847dadfc543
Salmanulfaris12 <160258317+Salmanulfaris12@users.noreply.github.com>
2024-08-28 16:48:10 +0530

    Delete Length of Lastword.js

--- Length of Lastword.js
+++
@@ -1,8 +0,0 @@
-var LengthOfLastWord=function(s){
-    s=s.trim().split(" ")
-     let res=s.filter((value)=>{
-       return value==s[s.length-1]
-    })
-     return res.toString().length
-}
-console.log(LengthOfLastWord("  hello world  "))
