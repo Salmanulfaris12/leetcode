3422e3f2e60e164c7a67ff90252a3c818264b52c
Salmanulfaris12 <160258317+Salmanulfaris12@users.noreply.github.com>
2024-08-28 16:48:41 +0530

    Delete validPalindrome.js

--- validPalindrome.js
+++
@@ -1,6 +0,0 @@
-var isPalindrome=function(s){
-    let normalizedString=s.toLowerCase().replace(/[^a-z0-9]/g,'')
-    let reverseString=normalizedString.split("").reverse().join("")
-    return normalizedString==reverseString
-}
-console.log(isPalindrome("A man, a plan, a canal: Panama"));
