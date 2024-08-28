cbb720555521384c89b006d544039d541e9daa08
Salmanulfaris12 <160258317+Salmanulfaris12@users.noreply.github.com>
2024-08-28 16:50:46 +0530

    Delete reduce.js

--- reduce.js
+++
@@ -1,9 +0,0 @@
-var reduce = function(nums, fn, init) {
-   let b=init
-   for(i=0;i<nums.length;i++){
-   b=fn(b,nums[i])
-   } 
-   return b;
-};
-let c= function sum(accum, curr) { return accum +curr*curr; }
-console.log(reduce([1,2,3,4],c,10));
