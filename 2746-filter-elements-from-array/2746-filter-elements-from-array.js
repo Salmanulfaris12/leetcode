eb4847dd79aeff1dca57b7dad7ac2f7388e9b2e9
Salmanulfaris12 <160258317+Salmanulfaris12@users.noreply.github.com>
2024-08-28 16:56:21 +0530

    Time: 58 ms (26.99%), Space: 48.9 MB (55.88%) - LeetHub

---
+++ 2746-filter-elements-from-array/2746-filter-elements-from-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let a=[]
+   for(i=0;i<arr.length;i++){
+    b=fn(arr[i],i)
+   if(b){
+            a.push(arr[i])
+        }
+   } 
+   return a
+};
+
