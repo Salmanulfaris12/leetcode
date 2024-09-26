052070c119de68b2864a33d1d137f7f511141067
Salmanulfaris12 <160258317+Salmanulfaris12@users.noreply.github.com>
2024-09-26 10:15:48 +0530

    Time: 56 ms (44.91%), Space: 49.3 MB (49.12%) - LeetHub

---
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var sumOfUnique = function(nums) {
+    sum=0
+   for (i=0;i<nums.length;i++){
+    if(nums.indexOf(nums[i])===nums.lastIndexOf(nums[i])){
+        sum+=nums[i]
+    }
+   }
+   return sum
+};
