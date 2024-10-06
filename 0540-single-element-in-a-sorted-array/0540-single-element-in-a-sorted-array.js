a816a539a29f2890a2dfe78a388effa36aba2f8b
Salmanulfaris12 <160258317+Salmanulfaris12@users.noreply.github.com>
2024-10-06 09:02:24 +0530

    Time: 55 ms (52.81%), Space: 52.2 MB (32.65%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function(nums) {
+    for(i=0;i<nums.length;i++){
+        if(nums[i-1]!==nums[i]&&nums[i]!==nums[i+1]){
+           return nums[i] 
+        }  
+    }
+};
