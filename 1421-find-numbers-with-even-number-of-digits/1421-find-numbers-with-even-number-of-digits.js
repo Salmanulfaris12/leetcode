ac8b401ea68754d415a005cf91037d52e9c9d50f
Salmanulfaris12 <160258317+Salmanulfaris12@users.noreply.github.com>
2024-10-26 09:41:52 +0530

    Time: 1 ms (98.49%), Space: 51.3 MB (8.7%) - LeetHub

---
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+    let count=0
+    for(x of nums){
+        if(x.toString().length%2==0){
+            count++
+        }
+    }
+    return count
+};
